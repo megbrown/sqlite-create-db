6d21ee463482ad94c9aa5e09bf1c072b4c3b890e
Megan B <brown.m.j@outlook.com>
2017-08-29 15:47:27 -0500

    fixed error

--- sqlite3-intro.js
+++ sqlite3-intro.js
@@ -8,7 +8,14 @@ const errorHandler = (err) => {
   };
 };
 
-db.run("CREATE TABLE IF NOT EXISTS employees (id INT, first TEXT, last TEXT)");
+
+db.run(`DROP TABLE IF EXISTS employees`);
+
+const createEmployeesTable = () => {
+  db.run("CREATE TABLE IF NOT EXISTS employees (id INT, first TEXT, last TEXT)");
+}
+createEmployeesTable();
+
 
 db.run("INSERT INTO employees (id, first, last) VALUES (1, 'Michael', 'Scott')", errorHandler);
 db.run("INSERT INTO employees VALUES (2, 'Jim', 'Halpert')", errorHandler);
@@ -23,11 +30,11 @@ employeeArray.forEach((emp) => {
   db.run(`INSERT INTO employees VALUES (${emp.id}, '${emp.firstName}', '${emp.lastName}')`);
 });
 
-db.all("SELECT * FROM employees", (err, allRows) => {
-		errorHandler(err);
-	  allRows.forEach(employee => {
-    console.log(employee.id, employee.first + ' ' + employee.last);
-  });
+db.each('SELECT id, first, last FROM employees', (err, {id, first, last}) => {
+	if (err) {
+    return console.log('err', err.toString());
+  }
+  console.log(`${id} ${first} ${last}`);
 });
 
 db.close(err => {
